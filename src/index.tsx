1ff056cbe3b7af8e1bf06d4921d56f89dc087191
Sarah <sarahghobj@hotmail.com>
2022-05-31 03:40:47 +0200

    s

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-      <BrowserRouter basename={'/portfolio'}>
+      <BrowserRouter basename={'/portfolio/'}>
           <App />
       </BrowserRouter>
   </React.StrictMode>
